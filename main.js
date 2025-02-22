fee27eb94e70d1a7c513a18221df4993d15da669
MariiaKovalenk <sulyyymash@gmail.com>
2025-02-22 19:37:40 +0200

    homework4.2: compare number

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let answer = prompt('Hello how is goin?')
+let answer = prompt('Enter your number'); // 375
+let number1 = answer % 10; // 5
+let number2 = (answer - number1) / 10 % 10; // 7
+let number3 = (answer - number1 - (number2 * 10)) / 100; // 3
 
-
-
-console.log(45);
-console.log(answer);
+console.log(`Each number are equal to another one? Answer: ${number1 === number2 && number3 === number1}`);
+console.log(`Some number are equal? Answer: ${number1 ===  number2 || number1 === number3 || number2 ===  number3}`);
