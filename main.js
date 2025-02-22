ecbd204d39c08c77153cab8c7b796387ddd0ce08
MariiaKovalenk <sulyyymash@gmail.com>
2025-02-22 18:47:40 +0200

    homework3.3: answer and return reversed number

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let answer = prompt('Hello how is goin?')
+let answer = prompt('Enter your number'); // 375
+let number1 = answer % 10; // 5
+let number2 = (answer - number1) / 10 % 10; // 7
+let number3 = (answer - number1 - (number2 * 10)) / 100; // 3
 
-
-
-console.log(45);
-console.log(answer);
+console.log(`${number1} ${number2} ${number3}`);
