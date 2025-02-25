cec7fba7cdc10b795303e38dcc0a4be108f1c4f2
MariiaKovalenk <sulyyymash@gmail.com>
2025-02-25 17:01:10 +0200

    homework 4.4:rewritten code in using switch-casе

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let answer = prompt('Hello how is goin?')
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr)):
+        console.log(' number is Ba_NaN')
+        break;
+    default: console.log('OK!');
+}
 
 
-
-console.log(45);
-console.log(answer);
