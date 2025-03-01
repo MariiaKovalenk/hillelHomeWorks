4541400c42771dcb849be9cd777818dbaf6e7387
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-01 16:59:57 +0200

    homework5.4 Determine whether number is prime

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-let answer = prompt('Hello how is goin?')
+let number = Number (prompt("Enter your number"));
 
+function isRight(inputNumber) {
+    let result = true;
 
+    if ( inputNumber <= 1) {
+        result = false;
+    }
 
-console.log(45);
-console.log(answer);
+    for (let i = 2; i < inputNumber; i++) {
+        if (inputNumber % i === 0) {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+console.log(isRight(number));
+
+//(простим називається число, більше 1, що не має інших дільників, крім 1 і себе).
+// 3 - i=2; 2 <= 3 (true); 2++ - 3 % 2 = 1(false); output: -;
+// 3 - i=3; 3 < 3 (false);
