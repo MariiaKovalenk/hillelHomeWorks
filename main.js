b22c0c40d4f2ed8683266babdf2dbb7fe5a51e80
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-10 17:41:43 +0200

    homework7.2 - multiply numbers

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const arr = [1, 3, 4, 6, 2, 5, 7];
-
-function removeElement(array, item) {
-    for (let i = 0; i < array.length; i++) {
-        if (item === array[i]) {
-            array.splice(i, 1);
-            break;
-        }
+function name(a) {
+    return function (b) {
+        return a * b;
     }
 }
-removeElement(arr, 4);
-console.log(arr);
+console.log(name(5)(2));
