9dc18183f719a61cdbf686c49dd50d202ac8f07b
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-11 18:15:42 +0200

    homework7.1: closures and context in sum function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function sum(a) {
-        this.totalsum = (this.totalsum === undefined ? 0 : this.totalsum);
-        this.totalsum = a + this.totalsum;
-        return this.totalsum;
+function makeSumCalculator() {
+        let totalSum = 0;
+        return (a) => {
+                totalSum += a;
+                return totalSum;
+        }
 }
-
+const sum = makeSumCalculator();
 console.log(sum(4)); // 4
 
 console.log(sum(6)); // 10
@@ -11,3 +13,18 @@ console.log(sum(6)); // 10
 console.log(sum(10)); // 20
 
 console.log(sum(7)); // 27
+
+
+// function sum(a) {
+//         this.totalsum = (this.totalsum === undefined ? 0 : this.totalsum);
+//         this.totalsum = a + this.totalsum;
+//         return this.totalsum;
+// }
+//
+// console.log(sum(4)); // 4
+//
+// console.log(sum(6)); // 10
+//
+// console.log(sum(10)); // 20
+//
+// console.log(sum(7)); // 27
