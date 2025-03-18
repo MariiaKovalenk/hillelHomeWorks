54bb70694db1008408e066c0d4c747ddf26ff7a3
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-18 13:47:02 +0200

    homework10.1-get information from obgect

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-let ladder = {
-        step: 0,
-        up: function () { // підніматиме вас на одну сходинку
-                this.step = this.step + 1;
-                return this;
-        },
-        down: function () { // опускатиме вас на одну сходинку
-                this.step = this.step - 1;
-                return this;
-        },
-        showStep: function () { // показує поточну сходинку
-                console.log(this.step);
-        }
+const user = {
+    name: 'Alise',
+    age: '25',
+    location: 'London',
+
+    getData: function () {
+        const userData = `Name: ${this.name}; Age: ${this.age}; Location: ${this.location}`;
+        console.log(userData);
+        return userData;
+    }
 };
 
-ladder.up().up().down().showStep(); // 1
+user.getData();
