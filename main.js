1bd769a9b6abac05107901d7680c16e2f2272d58
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-22 15:22:23 +0200

    homework11.3: add random image generator'

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const arr = [1, 3, 4, 6, 2, 5, 7];
-
-function removeElement(array, item) {
-    for (let i = 0; i < array.length; i++) {
-        if (item === array[i]) {
-            array.splice(i, 1);
-            break;
-        }
-    }
-}
-removeElement(arr, 4);
-console.log(arr);
+document.getElementById('button-random-image').addEventListener('click', ((arguments) => {
+    const imageElement = document.getElementById('image-random');
+    imageElement.src = `images/download${Math.floor(Math.random() * 10) + 1}.jpg`;
+}));
