7e6d02012d5f793f7070b3ffbfe08d36ce7ddef4
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-22 15:27:36 +0200

    homework11.3: remove image and change from 1-10 to 1-9

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 document.getElementById('button-random-image').addEventListener('click', ((arguments) => {
     const imageElement = document.getElementById('image-random');
-    imageElement.src = `images/download${Math.floor(Math.random() * 10) + 1}.jpg`;
+    imageElement.src = `images/download${Math.floor(Math.random() * 9) + 1}.jpg`;
 }));
