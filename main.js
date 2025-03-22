e31a7984cdf3d1ecf4d03d5c44839db717f0606b
MariiaKovalenk <sulyyymash@gmail.com>
2025-03-22 14:48:27 +0200

    homework11.1: add pifagore table

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-const phoneBook = {
-    contacts: [
-        {name:'Alice', phone: '+3806549593', email: 'alise@fgs.com'},
-        {name:'Bob', phone: '+3806549544', email: 'bob@fgs.com'},
-        {name:'Bob', phone: '+3806549544', email: 'bob123@fgs.com'}],
-    getContactInfo(name) {
-        return this.contacts.find((element) => {
-            return name === element.name;
-        });
-        // this.contacts.filter((element) => {
-        //     return name === element.name;
-        // });
-    },
-    addContact(name, phone, email){
-        this.contacts.push({name: name, phone: phone, email: email });
-        // this.contacts.push({ name, phone, email });
-    }
+function getPiphagoreTable(x) {
+    const result = new Map();
+    Array.from({ length: x }, (_, i) => i + 1).forEach((number) => {
+        for (let i = 1; i <= x; i++) {
+            result.set(number, [...(result.get(number) ?? []), number * i]);
+        }
+    });
+
+    return Object.fromEntries(result);
 }
 
+document.getElementById('pifagor_calculate_button').addEventListener('click', (event) => {
+    const pifagoreTableElement = document.getElementById('pifagor-table');
 
+    // clear previous result
+    pifagoreTableElement.replaceChildren();
 
+    const enteredNumber = Number(document.getElementById('pifagor_number_input').value);
+    const table = getPiphagoreTable(enteredNumber);
 
-phoneBook.addContact("Anna", "+380965556868", "jsfa@gm.com");
-phoneBook.getContactInfo("Bob");
+    const headerRow = document.createElement('tr');
+
+    const tableKeysArray = Object.keys(table);
+    tableKeysArray.forEach((tableKey) => {
+        const tableHeaderCellElement = document.createElement('td');
+        tableHeaderCellElement.textContent = tableKey;
+        headerRow.append(tableHeaderCellElement);
+    });
+    pifagoreTableElement.append(headerRow);
+
+    tableKeysArray.forEach((tableKey) => {
+        const contentRow = document.createElement('tr');
+        const tableKeyValues = Object.values(table[tableKey]);
+
+        tableKeyValues.forEach((tableKeyValue) => {
+            const tableKeyValueCellElement = document.createElement('td');
+            tableKeyValueCellElement.textContent = tableKeyValue;
+            contentRow.append(tableKeyValueCellElement);
+        });
+        pifagoreTableElement.append(contentRow);
+    });
+})
