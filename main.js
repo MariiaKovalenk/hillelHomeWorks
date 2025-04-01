e29483e8d6113040acbdc9e2e50780a2f4f7241f
MariiaKovalenk <sulyyymash@gmail.com>
2025-04-01 16:02:50 +0300

    homework12.2

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const arr = [1, 3, 4, 6, 2, 5, 7];
-
-function removeElement(array, item) {
-    for (let i = 0; i < array.length; i++) {
-        if (item === array[i]) {
-            array.splice(i, 1);
-            break;
-        }
-    }
+function buttonsWrapperClicked(event) {
+    console.log(event.target.id)
+    alert(`Натиснуто кнопку: ${''}`)
 }
-removeElement(arr, 4);
-console.log(arr);
