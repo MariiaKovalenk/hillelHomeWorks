97362302d33e0511bf2e8aafa086c9f29b6107cd
MariiaKovalenk <sulyyymash@gmail.com>
2025-04-01 18:04:52 +0300

    homework12.3 to-do list

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function buttonsWrapperClicked(event) {
-    console.log(event.target.id)
-    alert(`Натиснуто кнопку: ${''}`)
-}
+const tasksList = document.querySelector("#tasks-list");
+const addNewInput = document.querySelector("#new-task");
+const addNewBtn = document.querySelector("#add-new");
+
+tasksList.addEventListener("click",(e) => {
+    if(e.target.classList.contains("delete-button")) {
+        e.target.parentElement.remove();
+    }
+    e.stopPropagation();
+})
+
+addNewBtn.addEventListener("click", () => {
+    const task = addNewInput.value.trim();
+    if(task.trim()) {
+        const li = document.createElement("li");
+        li.innerHTML = `${task} <button class="delete-button">Видалити</button>`;
+        tasksList.appendChild(li);
+    }
+
+    addNewInput.value = "";
+})
+
